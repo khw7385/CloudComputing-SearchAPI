6f22e8ae9f1495b912ff06cc0a57bb43779db8bb
hyunwon <khw73850@gmail.com>
2024-05-23 00:04:49 +0900

    Feat: 트라이 검색 기능 구현

--- src/main/java/com/example/searchapi/trie/Phoneme.java
+++ src/main/java/com/example/searchapi/trie/Phoneme.java
@@ -4,19 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Phoneme {
-    private static final List<Character> firstSoundList = Arrays.asList('ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ' , 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ');
-    private static final List<Character> midSoundList = Arrays.asList('ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ');
-    private static final List<Character> lastSoundList = Arrays.asList(null, 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ');
+    private static final List<Character> firstSoundList = Arrays.asList('ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ' , 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'); // 19개
+    private static final List<Character> midSoundList = Arrays.asList('ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'); // 21개
+    private static final List<Character> lastSoundList = Arrays.asList(null, 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'); // 28개
 
-    public static Character getFirstSound(int idx){
-        return firstSoundList.get(idx);
-    }
-
-    public static Character getMidSound(int idx){
-        return midSoundList.get(idx);
-    }
-
-    public static Character getLastSound(int idx){
-        return lastSoundList.get(idx);
+    public static Integer getFirstSoundIdx(char ch){
+        int size = firstSoundList.size();
+        return Arrays.binarySearch(firstSoundList.toArray(), ch);
     }
 }
--- src/main/java/com/example/searchapi/trie/Trie.java
+++ src/main/java/com/example/searchapi/trie/Trie.java
@@ -1,37 +1,133 @@
 package com.example.searchapi.trie;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
+import com.example.searchapi.exception.EnglishTagFoundException;
+
+import java.util.*;
 
 public class Trie {
     private final Node root = new Node();
+    private static final Integer HANGUL_BASE_CODE = 44032;
+    private static final Integer FIRST_SOUND_SIZE = 19;
+    private static final Integer MID_SOUND_SIZE = 21;
+    private static final Integer LAST_SOUND_SIZE = 28;
+    private static final Integer LAST_SOUND_NULL_INDEX = 40;
+
     private void insert(String tag){
         Node node = this.root;
+
         for (int i = 0; i < tag.length(); i++){
             char ch = tag.charAt(i);
 
-            int last_idx = (ch - 44032) % 28;
-            int mid_idx = ((ch - 44032) / 28) % 21;
-            int first_idx = ((ch - 44032) / 28) / 21;
+            Integer firstSound = this.getFirstSoundIdx(ch);
+            Integer midSound = this.getMidSoundIdx(ch);
+            Integer lastSound = this.getLastSoundIdx(ch);
 
-            Character firstSound = Phoneme.getFirstSound(first_idx);
-            Character midSound = Phoneme.getMidSound(mid_idx);
-            Character lastSound = Phoneme.getLastSound(last_idx);
-
-            node = node.findChildNode(firstSound);
-            node = node.findChildNode(midSound);
-
-            if(lastSound != null){
-                node = node.findChildNode(lastSound);
-            }
+            node = createNodeFromLetter(node, firstSound, midSound, lastSound);
         }
         node.setData(tag);
         node.setLast(true);
     }
-    public Trie insertAll(List<String> tagList){
+
+    private Integer getFirstSoundIdx(char ch) {
+        return ((ch - HANGUL_BASE_CODE) / LAST_SOUND_SIZE) / MID_SOUND_SIZE; // 0 ~ 18
+    }
+
+    private Integer getMidSoundIdx(char ch) {
+        return ((ch - HANGUL_BASE_CODE) / LAST_SOUND_SIZE) % MID_SOUND_SIZE + FIRST_SOUND_SIZE; // 19 ~ 39
+    }
+
+    private Integer getLastSoundIdx(char ch) {
+        return (ch - HANGUL_BASE_CODE) % LAST_SOUND_SIZE + FIRST_SOUND_SIZE + MID_SOUND_SIZE; // 40 ~ 67
+    }
+
+    private Node createNodeFromLetter(Node node, Integer firstSound, Integer midSound, Integer lastSound) {
+        node = node.addChildNode(firstSound);
+        node = node.addChildNode(midSound);
+
+        if(lastSound != null){
+            node = node.addChildNode(lastSound);
+        }
+        return node;
+    }
+
+    private boolean checkOnlyFirstSound(char ch) {
+        return ch <= 'ㅎ';
+    }
+
+    private Node searchNodeFromLetter(Node node, Integer firstSound, Integer midSound, Integer lastSound) {
+        if(node.findNode(firstSound)){
+            node = node.getNextNode(firstSound);
+
+            if(node.findNode(midSound)){
+                node = node.getNextNode(midSound);
+
+                if(!lastSound.equals(LAST_SOUND_NULL_INDEX)){
+                    if(node.findNode(lastSound)){
+                        node = node.getNextNode(lastSound);
+                        return node;
+                    }
+                    return null;
+                }
+                return node;
+            }
+            return null;
+        }
+        return null;
+    }
+
+    private Node navigateToNode(Node node ,String searchText) {
+        for (int i = 0; i < searchText.length(); i++) {
+            char ch = searchText.charAt(i);
+
+            if(checkOnlyFirstSound(ch)){
+                Integer idx = Phoneme.getFirstSoundIdx(ch);
+                node = node.getNextNode(idx);
+            }else{
+                Integer firstSound = this.getFirstSoundIdx(ch);
+                Integer midSound = this.getMidSoundIdx(ch);
+                Integer lastSound = this.getLastSoundIdx(ch);
+                node = searchNodeFromLetter(node, firstSound, midSound, lastSound);
+            }
+            if(node == null) return null;
+        }
+        return node;
+    }
+
+    private void bfs(Node node, List<String> tagList) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+
+        while(!queue.isEmpty()){
+            node = queue.remove();
+            List<Node> nextNodes = node.getChildNodes();
+
+            for (Node nextNode : nextNodes){
+                if(nextNode.isLast()){
+                    tagList.add(nextNode.getData());
+                }
+                queue.add(nextNode);
+            }
+        }
+    }
+
+    public void insertAll(List<String> tagList){
+        tagList.forEach(tag-> {
+            if (tag.matches(".*[a-zA-Z].*")){
+                throw new EnglishTagFoundException("태그 안에 영어가 존재합니다.");
+            }
+        });
         tagList.forEach(this::insert);
-        return this;
+    }
+
+    public List<String> searchTags(String searchText){
+        List<String> tagList = new ArrayList<>();
+        Node node = root;
+        node = navigateToNode(node ,searchText);
+
+        if(node == null) return tagList;
+
+        bfs(node, tagList);
+
+        return tagList;
     }
 }
