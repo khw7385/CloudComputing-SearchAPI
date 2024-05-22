5468cb89747117886872ede65ff2a543b857739e
Untaini <untaini@naver.com>
2024-05-23 01:58:19 +0900

    feat: 클라이언트로부터 받은 token으로 auth 서버로부터 user id를 가져오는 기능 구현

---
+++ src/main/java/com/example/searchapi/configuration/InterceptorConfig.java
@@ -0,0 +1,30 @@
+package com.example.searchapi.configuration;
+
+import com.example.searchapi.interceptor.TokenInterceptor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    private final ObjectMapper objectMapper;
+
+    @Value("${auth.server-url}")
+    private String AuthServerURL;
+
+    @Value("${auth.api-url}")
+    private String AuthApiURL;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new TokenInterceptor(objectMapper, AuthServerURL, AuthApiURL))
+                .order(1)
+                .addPathPatterns("/api/search/**");
+    }
+
+}
---
+++ src/main/java/com/example/searchapi/interceptor/TokenInterceptor.java
@@ -0,0 +1,70 @@
+package com.example.searchapi.interceptor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.math.BigDecimal;
+
+@RequiredArgsConstructor
+public class TokenInterceptor implements HandlerInterceptor {
+
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String NOT_TOKEN_RESPONSE = "{ \"error\": \"토큰을 찾을 수 없습니다.\"}";
+
+
+    private final ObjectMapper objectMapper;
+
+    @Value("${auth.server-url}")
+    private final String AuthServerURL;
+
+    @Value("${auth.api-url}")
+    private final String AuthApiURL;
+
+    //유저 정보를 담을 record 생성
+    public record UserDetail(String id) {};
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader(AUTHORIZATION);
+
+        //Authorization header가 없다면 401 status code 반환
+        if (token == null) {
+            response.setStatus(401);
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(NOT_TOKEN_RESPONSE);
+
+            return false;
+        }
+
+        try {
+            //Auth 서버로부터 유저 ID를 가져옴
+            ResponseEntity<UserDetail> authResponse = RestClient.builder()
+                    .baseUrl(AuthServerURL + AuthApiURL)
+                    .defaultHeader(AUTHORIZATION, token)
+                    .build()
+                    .get()
+                    .retrieve()
+                    .toEntity(UserDetail.class);
+
+            //Auth 서버로부터 받은 정보를 파싱하고 BigDecimal로 바꿔서 Controller에게 전달함
+            request.setAttribute("userId", new BigDecimal(authResponse.getBody().id));
+
+        } catch (HttpClientErrorException errorException) { //만약 토큰 정보가 이상하면 Exception 발생
+            response.setStatus(errorException.getStatusCode().value());
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(errorException.getResponseBodyAsString());
+
+            return false;
+        }
+
+        return true;
+    }
+
+}
