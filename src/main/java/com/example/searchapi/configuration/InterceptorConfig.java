d82ab8c7e5081be455a82740070a360df2a172a2
Untaini <untaini@naver.com>
2024-05-23 02:05:09 +0900

    refactor: 사용하지 않는 import 문 삭제

--- src/main/java/com/example/searchapi/configuration/InterceptorConfig.java
+++ src/main/java/com/example/searchapi/configuration/InterceptorConfig.java
@@ -1,7 +1,6 @@
 package com.example.searchapi.configuration;
 
 import com.example.searchapi.interceptor.TokenInterceptor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
